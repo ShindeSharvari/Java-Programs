21833bda6425539635b0ca5c525485c4caadc525
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-05 20:59:38 +0530

    Add files via upload

---
+++ While1.java
@@ -0,0 +1,13 @@
+class While1 
+{
+	public static void main(String[] args) 
+	{
+		int count = 0;
+		while (count < 5)
+		{
+
+		  System.out.println("Good morning");
+		  count++;
+		}
+	}
+}
---
+++ While2.java
@@ -0,0 +1,13 @@
+class While2
+{
+	public static void main(String[] args) 
+	{
+		int count = 0;
+		while (count <= 100)
+		{
+
+		  System.out.println(count);
+		  count++;
+		}
+	}
+}
