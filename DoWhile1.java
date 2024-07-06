934fe51fe7fa2f74227cb828963f256f4103b012
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-06 23:14:01 +0530

    Add files via upload

---
+++ DoWhile1.java
@@ -0,0 +1,14 @@
+class DoWhile1 
+{
+	public static void main(String[] args) 
+	{
+		int count = 0;
+		do
+		{
+			System.out.println("Good Morning !");
+			count++;
+		}
+		while (count < 5);
+		
+	}
+}
