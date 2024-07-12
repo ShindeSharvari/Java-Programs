c412131bf739b7dc395c548403a86c9dff85f46d
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-12 20:38:05 +0530

    Add files via upload

---
+++ FibonacciCheck.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class FibonacciCheck
+{
+	public static void main(String[] args) 
+	{
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("Enter number: ");
+	  int num = sc.nextInt();
+	  int n1=0,n2=1,n3;
+
+	  for(int i=0;i<num;i++)
+		{
+		  n3=n1+n2;
+		  if(n1==num)
+			{
+			  System.out.println("Entered number is fibonacci number ");
+			  break;
+			}
+			else if(n1>num)
+			{
+              System.out.println("Entered number is not fibonacci number ");
+			  break;
+			}
+		  n1=n2;
+		  n2=n3;
+		}
+	}
+}
