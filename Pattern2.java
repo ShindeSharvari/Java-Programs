40112892262d3237b7383c42fd975b617924c32c
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-15 19:43:11 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+/*   
+
+*  *  *
+*  *  *
+*  *  *
+*  *  *
+
+*/
+
+class Pattern2
+{
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				System.out.print("*"+"  ");
+			}
+			System.out.println();
+		}
+	}
+}
