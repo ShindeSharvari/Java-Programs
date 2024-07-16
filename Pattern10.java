66faff0928cf71e0dabe89d6964a4f788c9d5ee5
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-16 19:48:20 +0530

    Add files via upload

---
+++ Pattern10.java
@@ -0,0 +1,37 @@
+/*
+
+                *
+              * *
+            *   *
+          *     *
+        *       *
+      *         *
+    *           *
+  *             *
+* * * * * * * * *
+
+*/
+
+class Pattern10
+{
+	public static void main(String[] args) 
+	{
+		int n=9;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==n-1 || j==n-1 || i+j==n-1)
+				{
+				   System.out.print("*"+" ");
+				} 
+				
+				else
+				{
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Pattern12.java
@@ -0,0 +1,36 @@
+/*
+
+	* * * * * * * * *
+	*             *
+	*           *
+	*         *
+	*       *
+	*     *
+	*   *
+	* *
+	*
+
+*/
+class Pattern12
+{
+	public static void main(String[] args) 
+	{
+		int n=9;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || i==0 || i+j==n-1)
+				{
+				   System.out.print("*"+" ");
+				} 
+				
+				else
+				{
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
