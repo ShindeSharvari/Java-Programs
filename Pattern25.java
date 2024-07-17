5ee6e2c76fb0c8ee4d5dcc5fbe1cfaed7bc1a30c
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-17 18:38:58 +0530

    Add files via upload

---
+++ Pattern25.java
@@ -0,0 +1,34 @@
+/*
+ 
+				  *
+				* *
+			  * * *
+			* * * *
+		  * * * * *
+		* * * * * *
+	  * * * * * * *
+	* * * * * * * *
+
+*/
+
+class Pattern25 
+{
+	public static void main(String[] args) 
+	{
+		int n=8;
+		for(int i=0;i<n;i++)
+		{   
+           // for (int space=0;space<n-i-1 ;space++ ) // This will also works.
+
+		    for (int space=1;space<n-i ;space++ )
+			{
+				System.out.print(" "+" ");
+			}
+			for(int star=0 ; star<=i;star++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+	}
+}
