74f5139f14e2ac97b84ff211ba2124bf6454a947
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-20 20:54:14 +0530

    Update Buzz.java

--- Buzz.java
+++ Buzz.java
@@ -15,11 +15,11 @@ class Buzz
 
 		if(num%7==0 || num%10==7)
 		{
-			System.out.println(+num+" is a buzz number");
+	            System.out.println(+num+" is a buzz number");
 		}
 		else
 		{
-           System.out.println(+num+" is a buzz number");
+                     System.out.println(+num+" is not a buzz number");
 		}
 	}
 }
