d85ecedb6ff1875c6ba15ac660d2ba9e2a2caab3
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-23 07:35:48 +0530

    Add files via upload

---
+++ ArmstrongNumberInRange.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class ArmstrongNumberInRange 
+{
+	public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first number:");
+		long start=sc.nextInt();
+
+		System.out.print("Enter the second number:");
+		long end=sc.nextInt();
+
+		while(start<=end)
+		{
+        long count=0;
+		for(long i=start ;i!=0;i/=10)
+		{
+			
+			count++;
+		}
+		long sum=0;
+		for(long i=start ;i!=0;i/=10)
+		{
+			long rem =i%10;
+			long pow = 1;
+			for(long j=0;j<count;j++)
+			{
+				pow=pow*rem;
+			}
+			sum=sum+pow;
+		}
+        if(start==sum)
+		{
+			System.out.println(start);
+		}
+		start++;
+	}
+  }
+}
