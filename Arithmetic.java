94b2aad04f410a62ce0c304596a6af9f45f942ff
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-25 18:43:15 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -2,11 +2,10 @@ class Arithmetic
 {
    public static void main(String[]args)
    {
-  int x=15,y=4,z=3;
-int exp1=x/y+y%x;
-int exp2=(x-y)/3+((z*2)-1);
+     int x=15,y=4,z=3;
+     int exp1=x/y+y%x;
+     int exp2=(x-y)/3+((z*2)-1);
      System.out.println(exp1);
-System.out.println(exp2);
-
+     System.out.println(exp2);
   }
 }
