e2559d47c13b8941c707392efbbdaf17d2d69cb7
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-27 21:49:45 +0530

    Update EvenFactors.java

--- EvenFactors.java
+++ EvenFactors.java
@@ -10,14 +10,14 @@ class EvenFactors
 	   int num = sc.nextInt();
 
 	   for(int i=1 ; i<=num ;i++)
-		{
-		   if(num%i==0)
-			{
-			   if(i%2==0)
-				{
-				   System.out.println(i);
-				}
-			}
-		}
+	   {
+	      if(num%i==0)
+	      {
+		 if(i%2==0)
+		 {
+		    System.out.println(i);
+		 }
+	     }
+	  }
 	}
 }
