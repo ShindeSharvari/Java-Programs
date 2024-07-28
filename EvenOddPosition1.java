8d8210be49ca8accda857e7915ed24f77bfb5e23
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-28 15:01:09 +0530

    Update EvenOddPosition1.java

--- EvenOddPosition1.java
+++ EvenOddPosition1.java
@@ -13,29 +13,28 @@ class EvenOddPosition1
 	  int count=0,even=0,product=1,rev=0;
 
 	  
-      while(num>0)
-		{
+         while(num>0)
+	 {
            rem=num%10;
            rev=(rev*10)+rem;
-		   num=num/10;
-		}
-	  System.out.println(rev);
-	   while(rev>0)
-		{
+	   num=num/10;
+	 }
+	System.out.println(rev);
+	while(rev>0)
+	{
            rem1=rev%10;
-		   count++;
-		   if(count%2==0)
-			{
-			   even=even+rem1;
-			}
-			else
-			{
-			  product=product*rem1;
-			}
-		   rev=rev/10;
-		}
-		System.out.println(even);
-		System.out.println(product);
-
+	   count++;
+	   if(count%2==0)
+	   {
+	     even=even+rem1;
+	   }
+	   else
+	   {
+	     product=product*rem1;
+	   }
+	   rev=rev/10;
+	}
+	System.out.println(even);
+	System.out.println(product);
 	}
 }
