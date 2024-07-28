500e804dd5a6425c2f56ced5392707931331454d
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-07-28 15:02:50 +0530

    Update EvenOddPositionSum.java

--- EvenOddPositionSum.java
+++ EvenOddPositionSum.java
@@ -10,26 +10,26 @@ class EvenOddPositionSum
 	  int count=0,evenSum=0,oddSum=0;
       
 	  while(num>0)
-		{
-           int rem=num%10;
-		   count++;
-		   if(count%2==0)
-			{
-			   evenSum=evenSum+rem;
-			}
-			else
-			{
-			  oddSum=oddSum+rem;
-			}
-		   num=num/10;
-		}
-		if(evenSum==oddSum)
-		{
-          System.out.println("The sum of even and odd position numbers are equal");
-		}
-		else
-		{
-          System.out.println("The sum of even and odd position numbers are not equal");
-		}
+	  {
+             int rem=num%10;
+	     count++;
+	     if(count%2==0)
+	     {
+		 evenSum=evenSum+rem;
+	     }
+	    else
+	    {
+		oddSum=oddSum+rem;
+	    }
+	    num=num/10;
+	}
+	if(evenSum==oddSum)
+	{
+            System.out.println("The sum of even and odd position numbers are equal");
+	}
+	else
+	{
+           System.out.println("The sum of even and odd position numbers are not equal");
+	}
 	}
 }
