04b7074aa1a9bf3c05fa13b2bb1eeb349e7058fc
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-08-09 19:10:40 +0530

    Add files via upload

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,21 +1,13 @@
-import java.util.Scanner;
-
 class EvenOdd
 {
-   public static void main(String[] args)
-    {
-
-     Scanner sc = new Scanner(System.in);
-     System.out.print("Enter the number to check even or odd :");
-     int num = sc.nextInt();
-
-    if(num % 2 == 0)
-    {
-     System.out.println("The entered number "+num+" is even number.");
-    }
-    else
-    {
-      System.out.println("The entered number "+num+" is odd number.");
-    }
-    }
+	public static void main(String[] args) 
+	{
+		int num =123;
+		if((num/2)*2==num)
+		{
+			System.out.println("Even Number");
+			return;
+		}
+		System.out.println("Odd Number");
+	}
 }
