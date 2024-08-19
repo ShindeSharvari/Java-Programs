d057cadd2e5a004bbf9bec4367ac387b4d23032b
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-08-19 10:33:21 +0530

    Add files via upload

---
+++ Laptop2.java
@@ -0,0 +1,25 @@
+class Laptop2 
+{
+	    String brand;
+		double price;
+		String processor;
+		int ram;
+		int rom;
+		String model;
+		String color;
+		double wt;
+
+		public void laptopInfo()
+	    {
+			System.out.println("Brand : "+brand);
+			System.out.println("Price : "+price);
+			System.out.println("Processor : "+processor);
+			System.out.println("Ram : "+ram);
+			System.out.println("Rom : "+rom);
+			System.out.println("Model : "+model);
+			System.out.println("Color : "+color);
+			System.out.println("Weight : "+wt);
+			System.out.println("=============================================");
+
+	  }
+}
