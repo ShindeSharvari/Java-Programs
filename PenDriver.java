d834dc06afe94f63e8f9a32cdae5466f020c3abe
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-09-04 21:39:40 +0530

    Add files via upload

---
+++ PenDriver.java
@@ -0,0 +1,10 @@
+class PenDriver 
+{
+	public static void main(String[] args) 
+	{
+		// Pen p1 = new Pen("Reynolds" ,"Plastic",20.0,"Black");
+		Pen p1 = new Pen("Reynolds" ,"Plastic",20.0,"Black","Blue",0.5,5,"Gel");
+		p1.displayPen();
+		p1.r1.showRefill();
+	}
+}
