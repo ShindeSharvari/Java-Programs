d4eef34e185ab8e6d92114949e4cd0e1c13aac07
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-09-20 08:03:09 +0530

    Add files via upload

---
+++ Game2.java
@@ -0,0 +1,11 @@
+class Game2 extends Game
+{
+	public void goBackward(int steps) 
+	{
+		System.out.println("Moved "+steps+" back");
+	}
+	public void goLeft(int steps) 
+	{
+		System.out.println("Moved "+steps+" left");
+	}
+}
