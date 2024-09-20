ea6ac84810168d9a9ae3eed677cdc472d389df63
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2024-09-20 08:02:49 +0530

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,19 @@
+class Game 
+{
+	public void goForward(int steps) 
+	{
+		System.out.println("Moved "+steps+" forward");
+	}
+	public void goBackward(int steps) 
+	{
+		System.out.println("Moved "+steps+" left");
+	}
+	public void goLeft(int steps) 
+	{
+		System.out.println("Moved "+steps+" back");
+	}
+	public void goRight(int steps) 
+	{
+		System.out.println("Moved "+steps+" right");
+	}
+}
